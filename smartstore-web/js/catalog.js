333a98337567b4124753c436a70f2865c78a0f2f
Gatnaron <oganyan.ivan2016@yandex.ru>
2024-05-28 11:39:30 +0300

    28.05.24/11:40

--- smartstore-web/js/catalog.js
+++ smartstore-web/js/catalog.js
@@ -20,12 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
     productGrid.addEventListener('click', function (event) {
         if (event.target.tagName === 'BUTTON') {
             const deviceId = event.target.getAttribute('data-device-id');
-            const userId = localStorage.getItem("userId");
-
-            if (!userId) {
-                alert("Пожалуйста, войдите в систему, чтобы добавить товар в корзину.");
-                return;
-            }
+            const userId = 1; // Assume the user ID is 1 for now
 
             fetch(`http://localhost:8080/baskets/${userId}/add`, {
                 method: 'POST',
@@ -34,17 +29,36 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
                 body: JSON.stringify({ deviceId })
             })
-                .then(response => {
-                    if (response.ok) {
-                        alert('Товар добавлен в корзину');
-                    } else {
-                        throw new Error("Ошибка при добавлении товара в корзину");
-                    }
+                .then(response => response.json())
+                .then(data => {
+                    alert('Товар добавлен в корзину');
                 })
                 .catch(error => {
                     console.error('Ошибка при добавлении товара в корзину:', error);
-                    alert('Не удалось добавить товар в корзину. Пожалуйста, попробуйте снова.');
                 });
         }
     });
+
+    const basketModal = document.getElementById("basket-modal");
+    const closeBasketModalButton = document.getElementById("close-basket-modal");
+    const userBasketIcon = document.getElementById("user-basket-icon");
+
+    // Открытие модального окна при клике на иконку корзины пользователя
+    userBasketIcon.addEventListener("click", function(event) {
+        event.preventDefault();
+        basketModal.style.display = "block";
+    });
+
+    // Закрытие модального окна при клике на кнопку закрытия
+    closeBasketModalButton.addEventListener("click", function(event) {
+        basketModal.style.display = "none";
+    });
+
+    // Закрытие модального окна при клике вне его области
+    window.addEventListener("click", function(event) {
+        if (event.target === basketModal) {
+            basketModal.style.display = "none";
+        }
+    });
 });
+
